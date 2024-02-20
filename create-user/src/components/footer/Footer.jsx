d4db37688a14ea47f67356c20ad338d432a5e91d
AbdullohSS <abdullohsoataliev@gmail.com>
2024-02-20 10:32:25 +0500

    USer qo'shildi

---
+++ create-user/src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="https://www.instagram.com/soataliev_abdulloh">Abdulloh Soataliev</a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
